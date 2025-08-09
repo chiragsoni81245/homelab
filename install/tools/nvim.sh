12033e28eb02008d0d0c6f6396cd6b2b5a2acc2b
Chirag Soni <chiragsoni81245@gmail.com>
2025-08-09 16:31:06 +0530

    fix nvim setup script

--- install/tools/nvim.sh
+++ install/tools/nvim.sh
@@ -8,6 +8,7 @@ if ! command -v nvim &>/dev/null; then
   curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
   sudo rm -rf /opt/nvim
   sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+  sudo mv /opt/nvim-linux-x86_64 /opt/nvim
 
   # Using my personal nvim configurations
   git clone https://github.com/chiragsoni81245/nvim.git ~/.config/nvim
