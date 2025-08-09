2a38143ac60f2e58a99f1a2c055f10b62683ff20
Chirag Soni <chiragsoni81245@gmail.com>
2025-08-09 22:28:31 +0530

    fix samba configurations

--- install/tools/samba.sh
+++ install/tools/samba.sh
@@ -4,14 +4,14 @@ if ! command -v smbd &>/dev/null; then
     sudo apt install -y samba
 
     sudo systemctl stop smbd
-
-    if [ ! -d '/etc/samba/shares' ]; then
-        # Copy Samba configurations
-        sudo mv /etc/samba/smb.conf /etc/samba/default_smb.conf
-        sudo mv ~/.local/share/homelab/config/sambsa/* /etc/samba/
-    fi
-
-    sudo systemctl start smbd
-    sudo systemctl enable smbd
 fi
+
+if [ ! -d '/etc/samba/share.conf' ]; then
+    # Copy Samba configurations
+    sudo mv /etc/samba/smb.conf /etc/samba/default_smb.conf
+    sudo mv ~/.local/share/homelab/config/sambsa/* /etc/samba/
+fi
+
+sudo systemctl start smbd
+sudo systemctl enable smbd
 
