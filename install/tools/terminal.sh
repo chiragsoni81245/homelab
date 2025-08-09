c2b5afe15ab82a806bd153de7ad91e2296546083
Chirag Soni <chiragsoni81245@gmail.com>
2025-08-10 03:07:56 +0530

    updated yq install method

--- install/tools/terminal.sh
+++ install/tools/terminal.sh
@@ -6,4 +6,5 @@ sudo apt install -y \
   wl-clipboard htop \
   man tldr less whois plocate bash-completion
 
-sudo snap install yq
+sudo wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/local/bin/yq
+sudo chmod +x /usr/local/bin/yq
