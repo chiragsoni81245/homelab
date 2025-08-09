346b4f9379199e2f0ce8171340eea0cd01c5e74d
Chirag Soni <chiragsoni81245@gmail.com>
2025-08-09 16:34:33 +0530

    nvim cleanup of tar file

--- install/tools/nvim.sh
+++ install/tools/nvim.sh
@@ -8,6 +8,7 @@ if ! command -v nvim &>/dev/null; then
   curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
   sudo rm -rf /opt/nvim
   sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+  sudo rm nvim-linux-x86_64.tar.gz 
   sudo mv /opt/nvim-linux-x86_64 /opt/nvim
 
   # Using my personal nvim configurations
