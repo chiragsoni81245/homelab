e264d10bb0c71d2b4672798d995d75fecc8f1c04
Chirag Soni <chiragsoni81245@gmail.com>
2025-08-10 03:57:20 +0530

    added sync-tool install

--- install/tools/terminal.sh
+++ install/tools/terminal.sh
@@ -10,3 +10,9 @@ if [ ! -f '/usr/local/bin/yq' ]; then
   sudo wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/local/bin/yq
   sudo chmod +x /usr/local/bin/yq
 fi
+
+# Install sync-tool
+if [ ! -f '/usr/local/bin/sync-tool' ]; then
+  sudo wget https://github.com/chiragsoni81245/sync-tool/releases/download/v1.1.0/sync-tool -O /usr/local/bin/sync-tool
+  sudo chmod +x /usr/local/bin/sync-tool
+fi
