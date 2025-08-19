f873b8813bbaa639324c5738acfcdec6f5320917
Chirag Soni <chiragsoni81245@gmail.com>
2025-08-19 06:13:40 +0530

    modified samba config

--- install/tools/samba.sh
+++ install/tools/samba.sh
@@ -2,16 +2,28 @@
 
 if ! command -v smbd &>/dev/null; then
     sudo apt install -y samba
-
-    sudo systemctl stop smbd
 fi
 
-if [ ! -d '/etc/samba/share.conf' ]; then
+sudo systemctl stop smbd
+
+SAMBA_CONFIGURATIONS=$(yq -o json ".samba" "$HOMELAB_CONFIG")
+
+
+# Rendering samba configurations from the go template engine
+echo $SAMBA_CONFIGURATIONS | tpl -f "~/.local/share/homelab/config/samba/smb.conf" > "~/.local/share/homelab/config/samba/.current/smb.conf"
+echo $SAMBA_CONFIGURATIONS | tpl -f "~/.local/share/homelab/config/samba/share.conf" > "~/.local/share/homelab/config/samba/.current/share.conf"
+
+if ! cmp -s "/etc/samba/smb.conf" "~/.local/share/homelab/config/samba/.current/smb.conf"; then
     # Copy Samba configurations
-    sudo cp /etc/samba/smb.conf /etc/samba/default_smb.conf
-    sudo cp -r ~/.local/share/homelab/config/samba/* /etc/samba/
+    sudo mv /etc/samba/smb.conf /etc/samba/last_backup_smb.conf
+    sudo cp ~/.local/share/homelab/config/samba/.current/smb.conf /etc/samba/smb.conf
+fi
+
+if ! cmp -s "/etc/samba/share.conf" "~/.local/share/homelab/config/samba/.current/share.conf"; then
+    # Copy Samba Shares configurations
+    sudo mv /etc/samba/smb.conf /etc/samba/last_backup_smb_share.conf
+    sudo cp ~/.local/share/homelab/config/samba/.current/share.conf /etc/samba/share.conf
 fi
 
 sudo systemctl start smbd
 sudo systemctl enable smbd
-
