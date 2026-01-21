2b66943c7d67199973f3629b54c2c0c0e0a1419a
Chirag Soni <52407451+chiragsoni81245@users.noreply.github.com>
2026-01-21 17:13:55 +0530

    fix formatting in ufw configure

--- install/tools/ufw.sh
+++ install/tools/ufw.sh
@@ -6,43 +6,43 @@ SSH_PORT="22"
 ENABLE_IPV6="$(yq '.ufw.enable_ipv6' "$HOMELAB_CONFIG")"
 ### ==================
 
-echo "[+] Installing UFW..."
+echo "\n[+] Installing UFW..."
 sudo apt update -y
 sudo apt install -y ufw
 
-echo "[+] Resetting UFW..."
+echo "\n[+] Resetting UFW..."
 sudo ufw --force reset
 
-echo "[+] Default policies..."
+echo "\n[+] Default policies..."
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
 
-echo "[+] Allow SSH from LAN only..."
+echo "\n[+] Allow SSH from LAN only..."
 sudo ufw allow from "$LAN_CIDR" to any port "$SSH_PORT" proto tcp comment "SSH from LAN"
 
-echo "[+] Allow HTTP/HTTPS..."
+echo "\n[+] Allow HTTP/HTTPS..."
 sudo ufw allow 80/tcp comment "HTTP"
 sudo ufw allow 443/tcp comment "HTTPS"
 
-echo "[+] Allow ICMP (ping)..."
+echo "\n[+] Allow ICMP (ping)..."
 sudo ufw allow proto icmp comment "Ping"
 
-echo "[+] Rate limit SSH..."
+echo "\n[+] Rate limit SSH..."
 sudo ufw limit "$SSH_PORT"/tcp comment "SSH rate limiting"
 
-echo "[+] IPv6 support..."
+echo "\n[+] IPv6 support..."
 sudo sed -i "s/^IPV6=.*/IPV6=${ENABLE_IPV6}/" /etc/default/ufw
 
-echo "[+] Patch after.rules for docker port bypass issue fix..."
+echo "\n[+] Patch after.rules for docker port bypass issue fix..."
 sudo cat ~/.local/share/homelab/config/ufw/after.rules >> /etc/ufw/after.rules
 
-echo "[+] Enabling UFW..."
+echo "\n[+] Enabling UFW..."
 sudo ufw --force enable
 
-echo "[+] Reloading config..."
+echo "\n[+] Reloading config..."
 sudo ufw reload
 
-echo "[+] Final UFW status:"
+echo "\n[+] Final UFW status:"
 sudo ufw status verbose
 
 echo "✅ UFW setup complete."
